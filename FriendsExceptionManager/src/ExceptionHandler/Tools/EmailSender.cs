39146cf39659650ec4e945d911c063bc19188594
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-02-26 00:04:42 +0330

    add email sender

---
+++ FriendsExceptionManager/src/ExceptionHandler/Tools/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler.Tools
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync();
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync() => await Task.Run(() =>
+        {
+            MailMessage mailMessage = new()
+            {
+                IsBodyHtml = true,
+                From = new("", ""),
+                Subject = "",
+                Body = ""
+            };
+            SmtpClient smtpClient = new()
+            {
+                Port = 25,
+                Credentials = new NetworkCredential("", ""),
+                EnableSsl = false
+            };
+
+            smtpClient.Send(mailMessage);
+        });
+    }
+}
