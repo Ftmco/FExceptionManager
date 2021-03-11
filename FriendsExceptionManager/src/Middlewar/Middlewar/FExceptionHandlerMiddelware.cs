5960ae56b36db9967e82c333aed16744d24d952d
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-03-12 01:01:52 +0330

    Change Middelware Class

---
+++ FriendsExceptionManager/src/Middlewar/Middlewar/FExceptionHandlerMiddelware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fteam.Middlewar
+{
+    public class FExceptionHandlerMiddelware
+    {
+        /// <summary>
+        /// Http Request Delegate
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        public FExceptionHandlerMiddelware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+        }
+    }
+}
---
+++ FriendsExceptionManager/src/Middlewar/Middlewar/MiddlewarExtentionExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace Fteam.Middlewar
+{
+    public static class FExceptionHandler
+    {
+        /// <summary>
+        /// Using Friends Asp.net core Exception Handler Midddleware
+        /// </summary>
+        /// <param name="app">IApplicationBuilder</param>
+        /// <returns>a reference to this instance after opration is compelete</returns>
+        public static IApplicationBuilder UseFExceptionHandler(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            return app.UseMiddleware<FExceptionHandlerMiddelware>();
+        }
+
+        public static IApplicationBuilder UseFExceptionHandler(this IApplicationBuilder app,Action<> action)
+        {
+
+        }
+    }
+}
